fa93a8cc17c3bc3ef8bc69877a7bee8a0a3abd3b
Konrad W <39115@student.atar.edu.pl>
2026-04-21 19:39:43 +0200

    Wpłowadzenie ustawień i dodanie wspałcia wielu języków ołaz ich zmiany, popława wałningów

--- app/src/main/java/com/example/stepfighter/ui/dungeon/DungeonActivity.kt
+++ app/src/main/java/com/example/stepfighter/ui/dungeon/DungeonActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.Image
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.zIndex
 import com.example.stepfighter.R
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -32,7 +33,6 @@ import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
